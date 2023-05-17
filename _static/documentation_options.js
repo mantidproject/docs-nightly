3d757ffc86c9ffd6c4ef4209a2ff8a704d0ceceb
mantid-builder <mantid-buildserver@mantidproject.org>
2023-05-17 05:27:04 +0000

    Publish nightly documentation

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '6.6.20230512.1314',
+    VERSION: '6.6.20230516.1658',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',

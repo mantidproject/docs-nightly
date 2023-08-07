63178b45d30a83bbc1eb6488a870ba638745ccd5
mantid-builder <mantid-buildserver@mantidproject.org>
2023-08-07 12:59:23 +0000

    Publish nightly documentation

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '6.7.20230801.1539',
+    VERSION: '6.7.20230804.1727',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',

8a7d8f1a2ae6120badfc4e04095387dfa694c943
mantid-builder <mantid-buildserver@mantidproject.org>
2026-07-11 06:07:04 +0000

    Publish nightly documentation from https://github.com/mantidproject/mantid/commit/dd698a864868befc25a26a987834bc394f275f77

--- _downloads/8883b5fce6bb68832acc5a55b7ae50c1/01_basic_plot_scripting-5.py
+++ _downloads/8883b5fce6bb68832acc5a55b7ae50c1/01_basic_plot_scripting-5.py
@@ -3,9 +3,8 @@
 from mantid.simpleapi import *
 import matplotlib.pyplot as plt
 
-# This file can be found in the Usage Examples folder, available
-# here https://www.mantidproject.org/installation/index#sample-data
-data = Load('PG3_733.nxs')
+data = Load('164198.nxs')
+data=ExtractSpectra(data, XMin=470, XMax=490, StartWorkspaceIndex=260, EndWorkspaceIndex=270)
 
 fig, ax = plt.subplots(subplot_kw={'projection':'mantid3d'})
 ax.plot_wireframe(data, color='green')
--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,5 +1,5 @@
 const DOCUMENTATION_OPTIONS = {
-    VERSION: '6.16.20260701.1953',
+    VERSION: '6.16.20260710.1810',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',

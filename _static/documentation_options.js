06349227619dd3e1847906949d878107eaff17bb
mantid-builder <mantid-buildserver@mantidproject.org>
2026-08-14 05:15:46 +0000

    Publish nightly documentation from https://github.com/mantidproject/mantid/commit/b0460639ee081adb83b400d663b83a05a62a2db1

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,5 +1,5 @@
 const DOCUMENTATION_OPTIONS = {
-    VERSION: '6.16.20260811.1408+uncommitted',
+    VERSION: '6.16.20260813.2148+uncommitted',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
